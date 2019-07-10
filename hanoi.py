c4ec269eedb2995486dd016f7d17abe14e7b3469
David Armendáriz <21047920+DavidArmendariz@users.noreply.github.com>
2019-07-09 23:02:37 -0500

    Delete hanoi.py

--- hanoi.py
+++
@@ -1,9 +0,0 @@
-def hanoi(n,pivote_inicial,pivote_final,pivote_auxiliar):
-    if n==1:
-        print(pivote_inicial+"->"+pivote_final)
-    else:
-        hanoi(n-1,pivote_inicial,pivote_auxiliar,pivote_final)
-        print(pivote_inicial+"->"+pivote_final)
-        hanoi(n-1,pivote_auxiliar,pivote_final,pivote_inicial)
-        
-        
